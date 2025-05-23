fe3943c8e1cf7e2ef7ed81bcf55f5eb6260ef275
i3oi3ka <i3oi3ka@ukr.net>
2025-05-23 22:38:00 +0300

    Enhance Redux store configuration with middleware for serializable checks

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filtersSlice";
 import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const contactsConfig = {
   key: "contacts",
@@ -13,6 +22,12 @@ const persistedContactReducer = persistReducer(contactsConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: { contacts: persistedContactReducer, filters: filterReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
